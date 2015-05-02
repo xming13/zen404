8cf5ae6bd08a8f49bf18a203c311ce6305d1fc19
Wei Seng <weiseng@redairship.com>
2015-05-02 13:37:38 +0800

    Add empty files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,6 +23,34 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/error-1', {
+        templateUrl: 'views/error-1.html',
+        controller: 'Error1Ctrl'
+      })
+      .when('/error-2', {
+        templateUrl: 'views/error-2.html',
+        controller: 'Error2Ctrl'
+      })
+      .when('/error-3', {
+        templateUrl: 'views/error-3.html',
+        controller: 'Error3Ctrl'
+      })
+      .when('/error-4', {
+        templateUrl: 'views/error-4.html',
+        controller: 'Error4Ctrl'
+      })
+      .when('/error-5', {
+        templateUrl: 'views/error-5.html',
+        controller: 'Error5Ctrl'
+      })
+      .when('/error-6', {
+        templateUrl: 'views/error-6.html',
+        controller: 'Error6Ctrl'
+      })
+      .when('/error-7', {
+        templateUrl: 'views/error-7.html',
+        controller: 'Error7Ctrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/error-5.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zen404App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the zen404App
+ */
+angular.module('zen404App')
+  .controller('Error5Ctrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
