bc257b7b6ac7e99b7d50601ceda9880e9fd655ce
Wei Seng <weiseng@redairship.com>
2015-05-02 15:27:21 +0800

    Fix bower for animate and textillate dependency

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -27,9 +27,9 @@ module.exports = function(config) {
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/letteringjs/jquery.lettering.js',
       'bower_components/textillate/jquery.textillate.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
