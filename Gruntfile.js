34d6662aa7dc36fda5a934ac926556e14aa44a86
Wei Seng <weiseng@redairship.com>
2015-05-03 14:54:48 +0800

    Update gruntfile to copy fontawesome fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -313,6 +313,11 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: 'bower_components/fontawesome',
+          src: 'fonts/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
